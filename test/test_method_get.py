6b57a7a59fa47187be9c3f1595d28cd017143ff3
Dagtret <sergeyaggo2015@gmail.com>
2025-02-12 15:43:01 +0300

    Добавил использование шагов allure в написанные тесты.

--- test/test_method_get.py
+++ test/test_method_get.py
@@ -1,6 +1,7 @@
 import httpx
 from jsonschema import validate
 from core.contracts import LIST_DATA_SCHEMA, LIST_USERS_DATA_SCHEMA
+import allure
 
 host = "https://reqres.in/"
 endpoint_list_users = "api/users?page=2"
@@ -13,46 +14,80 @@ email_ends = "@reqres.in"
 color_starts = "#"
 hyphen = "-"
 
+@allure.suite("Проверка запросов данных пользователей")
+@allure.title("Проверяем получение списка пользователей")
 def test_list_users():
-    response = httpx.get(host + endpoint_list_users)
-    assert response.status_code == 200
+    with allure.step(f"Делаем запрос по адресу: {host + endpoint_list_users}"):
+        response = httpx.get(host + endpoint_list_users)
+    with allure.step("Проверяем код ответа"):
+        assert response.status_code == 200
     data = response.json()["data"]
 
     for item in data:
-        validate(item, LIST_USERS_DATA_SCHEMA)
-        assert item["email"].endswith(email_ends)
-        assert str(item["id"]) in item["avatar"]
+        with allure.step("Проверяем элемент из списка"):
+            validate(item, LIST_USERS_DATA_SCHEMA)
+            with allure.step(f"Проверяем, что Email заканчивается на {email_ends}"):
+                assert item["email"].endswith(email_ends)
+            with allure.step(f'Проверяем, что значение id попадает в значение avatar'):
+                assert str(item["id"]) in item["avatar"]
 
+@allure.suite("Проверка запросов данных пользователей")
+@allure.title("Проверяем получение данных пользователя")
 def test_single_user():
-    response = httpx.get(host + endpoint_single_user)
-    assert response.status_code == 200
+    with allure.step(f"Делаем запрос по адресу {host + endpoint_single_user}"):
+        response = httpx.get(host + endpoint_single_user)
+    with allure.step("Проверяем код ответа"):
+        assert response.status_code == 200
     data = response.json()["data"]
     validate(data, LIST_USERS_DATA_SCHEMA)
-    assert data["email"].endswith(email_ends)
-    assert str(data["id"]) in data["avatar"]
+    with allure.step(f"Проверяем, что значение email заканчивается на {email_ends}"):
+        assert data["email"].endswith(email_ends)
+    with allure.step(f'Проверяем, что значение id попадает в значение avatar'):
+        assert str(data["id"]) in data["avatar"]
 
+@allure.suite("Проверка запросов данных пользователей")
+@allure.title(f"Проверяем какой ответ приходит по методу {endpoint_single_user_nf}")
 def test_single_user_nf():
-    response = httpx.get(host + endpoint_single_user_nf)
-    assert response.status_code == 404
+    with allure.step(f"Делаем запрос по адресу {host + endpoint_single_user_nf}"):
+        response = httpx.get(host + endpoint_single_user_nf)
+    with allure.step("Проверяем код ответа"):
+        assert response.status_code == 404
 
+@allure.suite("Проверка списка ресурсов")
+@allure.title("Проверяем общий списов ресурсов")
 def test_list_resource():
-    response = httpx.get(host + endpoint_list_resource)
-    assert response.status_code == 200
+    with allure.step(f"Делаем запрос по адресу: {host + endpoint_list_resource}"):
+        response = httpx.get(host + endpoint_list_resource)
+    with allure.step("Проверяем код ответа"):
+        assert response.status_code == 200
     data = response.json()["data"]
 
     for item in data:
-        validate(item, LIST_DATA_SCHEMA)
-        assert item["color"].startswith(color_starts)
-        assert hyphen in item["pantone_value"]
+        with allure.step("Проверяем элемент из списка"):
+            validate(item, LIST_DATA_SCHEMA)
+            with allure.step(f"Проверяем, что значение color начинается с {color_starts}"):
+                assert item["color"].startswith(color_starts)
+            with allure.step("Проверяем, что в pantone_value есть дефис"):
+                assert hyphen in item["pantone_value"]
 
+@allure.suite("Проверка списка ресурсов")
+@allure.title("Проверяем конкретный ресурс")
 def test_single_resource():
-    response = httpx.get(host + endpoint_single_resource)
-    assert response.status_code == 200
+    with allure.step(f"Делаем запрос по адресу: {host + endpoint_single_resource}"):
+        response = httpx.get(host + endpoint_single_resource)
+    with allure.step("Проверяем код ответа"):
+        assert response.status_code == 200
     data = response.json()["data"]
     validate(data, LIST_DATA_SCHEMA)
-    assert data["color"].startswith(color_starts)
-    assert hyphen in data["pantone_value"]
+    with allure.step(f"Проверяем, что значение color начинается с {color_starts}"):
+        assert data["color"].startswith(color_starts)
+    with allure.step("Проверяем, что в pantone_value есть дефис"):
+        assert hyphen in data["pantone_value"]
 
+@allure.suite("Проверка списка ресурсов")
+@allure.title(f"Проверяем какой ответ приходит по методу {endpoint_single_resource_nf}")
 def test_single_resource_nf():
-    response = httpx.get(host + endpoint_single_resource_nf)
-    assert response.status_code == 404
+    with allure.step(f"Делаем запрос по адресу: {host + endpoint_single_resource_nf}"):
+        response = httpx.get(host + endpoint_single_resource_nf)
+    with allure.step("Проверяем код ответа"):
+        assert response.status_code == 404
